cbd7d066f6d805f03800ff2d1f7340ef5558c33a
Carly <celevitz@gmail.com>
2023-07-02 17:07:33 -0700

    Add more statistics

--- UK02episodeSpecific.R
+++ UK02episodeSpecific.R
@@ -40,7 +40,8 @@ bootorder <- read.xlsx("UK02data.xlsx",sheet="Castaways") %>%
   select(castaway,castaway_id,result_number,stillin)
 
 ## win-loss record through this episode
-  winloss <- challenges %>%
+  winlossT <- challenges %>%
+    filter(outcome_type == "Tribal") %>%
     group_by(castaway,castaway_id,result) %>%
     summarise(n=n()) %>%
     pivot_wider(names_from = "result",values_from = n) %>%
@@ -48,9 +49,22 @@ bootorder <- read.xlsx("UK02data.xlsx",sheet="Castaways") %>%
                              ,TRUE ~ Won)
            ,Lost = case_when(is.na(Lost) ~ 0
                             ,TRUE ~ Lost)
-           ,Win_Loss = paste0(as.character(Won),"-",as.character(Lost) )
-           ,WinPercent = paste0(round(sum(Won)/sum(Won+Lost)*100,0),"%") ) %>%
-    select(castaway,castaway_id,Win_Loss,WinPercent)
+           ,Win_LossT = paste0(as.character(Won),"-",as.character(Lost) )
+           ,WinPercentT = paste0(round(sum(Won)/sum(Won+Lost)*100,0),"%") ) %>%
+    select(castaway,castaway_id,Win_LossT,WinPercentT)
+  
+  winlossI <- challenges %>%
+    filter(outcome_type == "Individual") %>%
+    group_by(castaway,castaway_id,result) %>%
+    summarise(n=n()) %>%
+    pivot_wider(names_from = "result",values_from = n) %>%
+    mutate(Won = case_when(is.na(Won) ~ 0
+                           ,TRUE ~ Won)
+           ,Lost = case_when(is.na(Lost) ~ 0
+                             ,TRUE ~ Lost)
+           ,Win_LossI = paste0(as.character(Won),"-",as.character(Lost) )
+           ,WinPercentI = paste0(round(sum(Won)/sum(Won+Lost)*100,0),"%") ) %>%
+    select(castaway,castaway_id,Win_LossI,WinPercentI)
   
   sitouts <- challenges %>%
     group_by(castaway,castaway_id) %>%
@@ -64,6 +78,16 @@ bootorder <- read.xlsx("UK02data.xlsx",sheet="Castaways") %>%
     summarise(TCattended = n()) %>%
     select(castaway,castaway_id,TCattended) 
   
+  tcsreceivedvotesat <- votehx %>%
+    filter(!(is.na(version)) & !(is.na(vote))) %>%
+    group_by(vote,vote_id) %>%
+    select(vote,vote_id,episode) %>%
+    distinct() %>%
+    mutate(receivedvotesat = n()) %>%
+    select(vote,vote_id,receivedvotesat) %>%
+    rename(castaway = vote,castaway_id = vote_id) %>%
+    distinct() 
+  
   numbervotesreceived <- votehx %>%
     filter(!(is.na(version)) & !(is.na(vote))) %>%
     group_by(vote,vote_id) %>%
@@ -72,6 +96,14 @@ bootorder <- read.xlsx("UK02data.xlsx",sheet="Castaways") %>%
     rename(castaway = vote,castaway_id = vote_id) %>%
     distinct() 
   
+  votesthatbootedthem <- votehx %>%
+    filter(!(is.na(version)) & !(is.na(vote))) %>%
+    filter(vote_id == voted_out_id) %>%
+    group_by(vote,vote_id) %>%
+    mutate(torchsnuffedvotes = n()) %>%
+    select(vote,vote_id,torchsnuffedvotes) %>%
+    rename(castaway = vote,castaway_id = vote_id) %>%
+    distinct() 
   
   successfulboots <- votehx %>%
     select(version,castaway,castaway_id,episode,order,vote_order
@@ -103,16 +135,23 @@ bootorder <- read.xlsx("UK02data.xlsx",sheet="Castaways") %>%
 ## Bring it all together
   combined <- tribes %>%
     full_join(bootorder) %>%
-    full_join(winloss) %>%
+    full_join(winlossT) %>%
+    full_join(winlossI) %>%
     full_join(sitouts) %>%
     full_join(attendance) %>%
     full_join(numbervotesreceived) %>%
+    full_join(tcsreceivedvotesat) %>%
+    full_join(votesthatbootedthem) %>%
     full_join(successfulboots) %>%
     full_join(conf)  %>%
     mutate(sitoutcount = ifelse(is.na(sitoutcount),0,sitoutcount)
            ,TCattended = ifelse(is.na(TCattended),0,TCattended)
+           ,receivedvotesat = ifelse(is.na(receivedvotesat),0
+                                     ,receivedvotesat)
            ,votesreceived = ifelse(is.na(votesreceived),0
                                         ,votesreceived)
+           ,torchsnuffedvotes = ifelse(is.na(torchsnuffedvotes),"NA"
+                                   ,torchsnuffedvotes)
            ,successfulpercent = ifelse(is.na(successfulpercent)
                                        ,"NA",successfulpercent)) 
   
@@ -127,32 +166,46 @@ bootorder <- read.xlsx("UK02data.xlsx",sheet="Castaways") %>%
   combined %>%
     ggplot(aes(x=0,y=0)) +
     geom_text(aes(x=rep(0,12),y=castaway,label=original_tribe,family=ft)) +
-    geom_text(aes(x=rep(10,12),y=castaway,label=Win_Loss,family=ft)
+    geom_text(aes(x=rep(10,12),y=castaway,label=Win_LossT,family=ft)
               ,size=textsize) +
-    geom_text(aes(x=rep(20,12),y=castaway,label=WinPercent,family=ft)
+    geom_text(aes(x=rep(20,12),y=castaway,label=WinPercentT,family=ft)
               ,size=textsize) +
     geom_text(aes(x=rep(30,12),y=castaway,label=sitoutcount,family=ft)
               ,size=textsize) +
-    geom_text(aes(x=rep(40,12),y=castaway,label=TCattended,family=ft)
+    geom_text(aes(x=rep(40,12),y=castaway,label=Win_LossI,family=ft)
               ,size=textsize) +
-    geom_text(aes(x=rep(50,12),y=castaway,label=votesreceived,family=ft)
+    geom_text(aes(x=rep(50,12),y=castaway,label=WinPercentI,family=ft)
               ,size=textsize) +
-    geom_text(aes(x=rep(60,12),y=castaway,label=successfulpercent,family=ft)
+    geom_text(aes(x=rep(60,12),y=castaway,label=TCattended,family=ft)
               ,size=textsize) +
-    geom_text(aes(x=rep(70,12),y=castaway,label=confessional_count,family=ft)
+    geom_text(aes(x=rep(70,12),y=castaway,label=receivedvotesat,family=ft)
               ,size=textsize) +
-    geom_text(aes(x=rep(85,12),y=castaway,label=confessional_time,family=ft)
+    geom_text(aes(x=rep(80,12),y=castaway,label=votesreceived,family=ft)
+              ,size=textsize) +
+    geom_text(aes(x=rep(90,12),y=castaway,label=torchsnuffedvotes,family=ft)
+              ,size=textsize) +
+    geom_text(aes(x=rep(100,12),y=castaway,label=successfulpercent,family=ft)
+              ,size=textsize) +
+    geom_text(aes(x=rep(110,12),y=castaway,label=confessional_count,family=ft)
+              ,size=textsize) +
+    geom_text(aes(x=rep(125,12),y=castaway,label=confessional_time,family=ft)
               ,size=textsize) +
     geom_vline(xintercept=5)+
-    geom_vline(xintercept=35)+
-    geom_vline(xintercept=65)+
-    scale_x_continuous(lim=c(0,90),name="",position="top"
-                       ,breaks=c(seq(0,70,10),85)
-                       ,labels=c("Starting tribe","Win-Loss","Win %","Sit-outs"
-                                 ,"Tribal Councils\nattended","Votes received"
+    geom_vline(xintercept=55)+
+    geom_vline(xintercept=105)+
+    scale_x_continuous(lim=c(0,130),name="",position="top"
+                       ,breaks=c(seq(0,110,10),125)
+                       ,labels=c("Starting tribe","Win-Loss\n(Tribal)"
+                                 ,"Win %\n(Tribal)","Sit-outs"
+                                 ,"Win-Loss\n(Individual)","Win %\n(Individual)"
+                                 ,"Tribal Councils\nattended"
+                                 ,"TCs at which\nreceived votes"
+                                 ,"Votes\nreceived","Votes that\nsent them home"
                                  ,"Successful\nboots (%)","Confessional\ncounts"
                                  ,"Confessional time\n(seconds)")) +
-    labs(title = "Survivor UK: Panama") +
+    labs(title = "Survivor UK: Panama"
+         ,subtitle = "Graphic by @carlylevitz"
+         ,caption = "Helen was eliminated based on a sudden-death trivia contest; Jonny and Susanna were never voted out") +
     theme_minimal() +
     theme(axis.ticks.x=element_blank()
           ,axis.line.x = element_blank() 
@@ -162,9 +215,9 @@ bootorder <- read.xlsx("UK02data.xlsx",sheet="Castaways") %>%
           ,panel.grid = element_blank()
           ,plot.title = element_text(family=ft,size=30,face="bold")
           ,plot.subtitle = element_text(family = ft)
-          ,plot.caption = element_text(family = ft))
+          ,plot.caption = element_text(family = ft,size = 18))
   
   
-  dev.print(png, file = "SurvivorUK02_Stats.png", width = 1200, height = 900)
+  dev.print(png, file = "SurvivorUK02_Stats.png", width = 1600, height = 900)
   dev.off()
   
